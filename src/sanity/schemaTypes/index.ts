e982072da7f0a7d877ecea51166a7d0d8a92f3d0
NimraRasheed37 <binteshaheen.na@gmail.com>
2025-01-24 17:08:44 +0500

    docs updated

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,7 +2,9 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { productSchema } from "./products";
 import { categorySchema } from "./categories";
+import { customerSchema } from "./customer";
+import { orderSchema } from "./orders";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema, categorySchema],
+  types: [productSchema, categorySchema, customerSchema , orderSchema ],
 };
